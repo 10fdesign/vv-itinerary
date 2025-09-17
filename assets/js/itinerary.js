a2a01166e33da3919d84135a51ac71fc72506b0a
Connor Deachman <connor@10fdesign.io>
2025-09-17 16:35:26 -0400

    minor changes

--- assets/js/itinerary.js
+++ assets/js/itinerary.js
@@ -249,6 +249,7 @@ window.addEventListener("DOMContentLoaded", function () {
 		for ( listing_data of listings_data.listings ) {
 			let id = parseInt( listing_data.url.split("/")[4] );
 			let _tile = document.createElement("div");
+			_tile.classList.add("listing-tile");
 			_tile.classList.add("flex");
 			_tile.classList.add("flex-col");
 			_tile.classList.add("relative");
