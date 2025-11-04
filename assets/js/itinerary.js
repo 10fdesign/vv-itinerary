c5948e20e97bd17486722dd46c01454c9078c5f9
connor <connor@10fdesign.io>
2025-11-04 12:15:43 -0500

    Added a fallback image for tiles.

--- assets/js/itinerary.js
+++ assets/js/itinerary.js
@@ -9,7 +9,7 @@ const UNDO_TEMPLATE = `
 `;
 
 const tile_template = `
-<img class="bg-red-50 aspect-3/2 w-full block object-cover tile-image" />
+<img src="/assets/img/fallback.jpg" class="bg-red-50 aspect-3/2 w-full block object-cover tile-image" />
 <a href="" class="bookmark-toggle block absolute top-4 right-4">
 	<svg class="h-8 fill-yellow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M305 151.1L320 171.8L335 151.1C360 116.5 400.2 96 442.9 96C516.4 96 576 155.6 576 229.1L576 231.7C576 343.9 436.1 474.2 363.1 529.9C350.7 539.3 335.5 544 320 544C304.5 544 289.2 539.4 276.9 529.9C203.9 474.2 64 343.9 64 231.7L64 229.1C64 155.6 123.6 96 197.1 96C239.8 96 280 116.5 305 151.1z"/></svg>
 </a>
